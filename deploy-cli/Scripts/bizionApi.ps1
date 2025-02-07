f5d5d458d45de2c896dc75983dbf1f7885241bd5
benitez <ebenitez@bizlogit.net>
2025-02-07 14:29:35 -0300

    fix: modifico script por posible error

--- deploy-cli/Scripts/bizionApi.ps1
+++ deploy-cli/Scripts/bizionApi.ps1
@@ -1,21 +1,27 @@
 # Rutas
-$carpetaOrigen = "C:\\Workspace\\Bizlogit\\bizion\\administration\\Admin.ApiService\\Admin.ApiService\\Admin.ApiService"
-$carpetaDest = "C:\\Workspace\\Bizlogit\\Deploy"
+$carpetaOrigen = "C:\\Users\\PC\\Desktop\\Workspace\\bizion\\administration\\Admin.ApiService\\Admin.ApiService\\Admin.ApiService"
+$carpetaDest = "C:\\Users\\PC\\Desktop\\Workspace\\Deploy"
 
+Write-Host "Verificando la existencia de la carpeta de destino..."
 if(-not(Test-Path $carpetaDest)) {
+    Write-Host "La carpeta de destino no existe. Creando la carpeta..."
     New-Item -ItemType Directory -Path $carpetaDest | Out-Null
 }
 
+Write-Host "Eliminando carpetas en la carpeta de origen..."
 # Busca y elimina carpetas
 Get-ChildItem $carpetaOrigen -Directory | Remove-Item -Recurse -Force
 
+Write-Host "Eliminando archivos por extensión en la carpeta de origen..."
 # Busca y Eliminar archivos por extension
-$extension = "*.xml", "*.pdb", "*.zip", "appsettings.json", "appsettings.Development.json", "log4net.config", "web.config", "app.config", "Konetic.DabaseContext.dll.config", "Konetic.Services.dll.config", "Konetic.Biz.dll.config", "Bizlogit.SCE.dll.config"
+$extensions = "*.xml", "*.pdb", "*.zip", "appsettings.json", "appsettings.Development.json", "log4net.config", "web.config", "app.config", "Konetic.DabaseContext.dll.config", "Konetic.Services.dll.config", "Konetic.Biz.dll.config", "Bizlogit.SCE.dll.config"
 
 foreach ($extension in $extensions) {
+    Write-Host "Eliminando archivos con la extensión $extension..."
     Get-ChildItem $carpetaOrigen -Recurse -Include $extension | Remove-Item -Force
 }
 
+Write-Host "Obteniendo los archivos filtrados..."
 # Obtener los archivos filtrados
 $archivosFiltrados = Get-ChildItem $carpetaOrigen -File
 
@@ -26,9 +32,12 @@ if(Test-Path $rutaZip) {
     Write-Host "El archivo zip '$nombreZip' ya existe. Por favor, elimine antes de crear uno nuevo."
 } else {
     if($archivosFiltrados.Count -gt 0) {
+        Write-Host "Comprimiendo archivos en $rutaZip..."
         Compress-Archive -Path $archivosFiltrados.FullName -DestinationPath $rutaZip
     }
     else {
-        Write-Host "No Hay archivos para comprimir"
+        Write-Host "No hay archivos para comprimir."
     }
 }
+
+Write-Host "Script de PowerShell finalizado."
