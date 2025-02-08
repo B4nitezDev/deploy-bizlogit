c1ec57b3172fb7acc3a41a1188868d00b33f6777
benitez <ebenitez@bizlogit.net>
2025-02-08 10:39:12 -0300

    refact: agrego lanzamiento de error

--- deploy-cli/Utils/FrontUtils.cs
+++ deploy-cli/Utils/FrontUtils.cs
@@ -5,12 +5,9 @@ public static class FrontUtil
     public static void BuildFrontMobile(string projectPath)
     {
         if (!Directory.Exists(projectPath))
-        {
             throw new DirectoryNotFoundException($"Error: La ruta '{projectPath}' no existe.");
-        }
-
+    
         Console.WriteLine("Modificando archivos antes del build...");
-
         ModifyIndexHtmlMobile(projectPath);
 
         RunCommand("npm", "run watch", projectPath);
@@ -21,9 +18,7 @@ public static class FrontUtil
     public static void BuildFromBizion(string projectPath)
     {
         if (!Directory.Exists(projectPath))
-        {
             throw new DirectoryNotFoundException($"Error: La ruta '{projectPath}' no existe.");
-        }
 
         Console.WriteLine("Modificando archivos antes del build...");
         ModifyIndexHtmlBizion(projectPath);
@@ -47,7 +42,6 @@ public static class FrontUtil
 
         string content = File.ReadAllText(indexPath);
 
-        // Reemplazar files
         content = content.Replace("<base href=\"/\" />", "<base href=\"/mobile\" />");
         content = content.Replace("    \"watch\": \"ng build --watch --configuration development\",\r\n", "    \"watch\": \"ng build --configuration production\",\r\n");
 
@@ -67,8 +61,7 @@ public static class FrontUtil
 
         string content = File.ReadAllText(indexPath);
 
-        // Reemplazar <base href="/"> por <base href="/mobile">
-        content = content.Replace("<base href=\"/\" />", "<base href=\"/bizion\" />");
+        content = content.Replace("<base href=\"/\">\r\n", "<base href=\"/bizion\" />");
 
         File.WriteAllText(indexPath, content);
         Console.WriteLine($"✔ Archivo modificado: {indexPath}");
@@ -79,11 +72,8 @@ public static class FrontUtil
         string enviromentPath = Path.Combine($"{projectPath}/src/environments/", "environment.prod.ts");
 
         if (!File.Exists(enviromentPath))
-        {
-            Console.WriteLine($"⚠ No se encontró {enviromentPath}, omitiendo modificación.");
-            return;
-        }
-
+            throw new InvalidOperationException($"⚠ No se encontró {enviromentPath}, omitiendo modificación.");
+        
         string content = File.ReadAllText(enviromentPath);
 
         content = content.Replace("    //baseUrl: '../apigateway/administration'\r\n", "    baseUrl: '../apigateway/administration'\r\n");
@@ -107,23 +97,21 @@ public static class FrontUtil
             CreateNoWindow = true
         };
 
-        using Process? process = Process.Start(psi);
-        if (process == null)
-        {
-            throw new InvalidOperationException($"Error: No se pudo iniciar el proceso '{command}'.");
-        }
-
+        using Process? process = Process.Start(psi) ?? 
+            throw new InvalidDataException($"Error: No se pudo iniciar el proceso '{command}'.");
+        
         string output = process.StandardOutput.ReadToEnd();
         string error = process.StandardError.ReadToEnd();
 
         process.WaitForExit();
 
-        if (!string.IsNullOrEmpty(output)) Console.WriteLine(output);
-        if (!string.IsNullOrEmpty(error)) Console.WriteLine($"Error:\n{error}");
+        if (!string.IsNullOrEmpty(output)) 
+            Console.WriteLine(output);
+
+        if (!string.IsNullOrEmpty(error)) 
+            Console.WriteLine($"Error:\n{error}");
 
         if (process.ExitCode != 0)
-        {
             throw new InvalidOperationException($"Error: El comando '{command} {arguments}' falló con el código de salida {process.ExitCode}.");
-        }
     }
 }
